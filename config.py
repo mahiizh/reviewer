377ab28dce7d758a3d7e3cd7a6c9e0d9da2ec2b7
mahiizh <mahil21arasan703@gmail.com>
2026-08-20 11:41:21 +0530

    new stores addition

--- config.py
+++ config.py
@@ -5,11 +5,7 @@ STORE_CONFIG = {
         "name": "Vilvah Store - Race Course, Coimbatore",
         "address": "ALG House, 167, near RK Photo Centre, Race Course, Coimbatore",
         "google_review_url": "https://g.page/r/CdXx0-RH-ibkEBM/review",
-    },
-    "brookefields": {
-        "name": "Vilvah Store - Brookefields Mall, Coimbatore",
-        "address": "67-71, Dr Krishnasamy Mudaliyar Rd, Coimbatore",
-        "google_review_url": "https://g.page/r/REPLACE_ME_BROOKEFIELDS/review",
+    
     },
     "tiruppur": {
         "name": "Vilvah Store - Tiruppur",
@@ -20,7 +16,57 @@ STORE_CONFIG = {
         "name": "Vilvah Store - Erode",
         "address": "Erode, Tamil Nadu",
         "google_review_url": " https://g.page/r/CX2DRjnxMm-LEBM/review",
-    }
+    },
+    "alwarpet": {
+        "name": "Vilvah Store - Alwarpet",
+        "address": "Alwarpet, Chennai, Tamil Nadu",
+        "google_review_url": "https://g.page/r/CRbNIULoE2vyEBM/review",
+    },
+    "bangalore": {
+        "name": "Vilvah Store - Bangalore",
+        "address": "Bangalore, Karnataka",
+        "google_review_url": "https://g.page/r/CfnUEhqEwue8EBM/review",
+    },
+    "brookefields-mall-coimbatore": {
+        "name": "Vilvah Store - Brookefields Mall Coimbatore",
+        "address": "Brookefields Mall, Coimbatore, Tamil Nadu",
+        "google_review_url": "https://g.page/r/CYJwC1Qo1AOFEBM/review",
+    },
+    "lulu-mall-kochi": {
+        "name": "Vilvah Store - Lulu Mall Kochi",
+        "address": "Lulu Mall, Kochi, Kerala",
+        "google_review_url": "https://g.page/r/Cf-zSzFpz2E_EBM/review",
+    },
+    "madurai": {
+        "name": "Vilvah Store - Madurai",
+        "address": "Madurai, Tamil Nadu",
+        "google_review_url": "https://g.page/r/CUPmug-yoV5LEBM/review",
+    },
+    "marina-mall-omr-chennai": {
+        "name": "Vilvah Store - Marina Mall OMR Chennai",
+        "address": "Marina Mall, OMR, Chennai, Tamil Nadu",
+        "google_review_url": "https://g.page/r/CQX1J8FIsLIgEBM/review",
+    },
+    "ea-mall-royapettah": {
+        "name": "Vilvah Store - EA Mall, Royapettah",
+        "address": "EA Mall, Royapettah, Chennai, Tamil Nadu",
+        "google_review_url": "https://g.page/r/CZQl5AB-YYtfEBM/review",
+    },
+    "phoenix-market-city-velachery": {
+        "name": "Vilvah Store - Phoenix Market City Velachery",
+        "address": "Phoenix Market City, Velachery, Chennai, Tamil Nadu",
+        "google_review_url": "https://g.page/r/CbJqHe396JneEBM/review",
+    },
+    "t-nagar-chennai": {
+        "name": "Vilvah Store - T Nagar Chennai",
+        "address": "T Nagar, Chennai, Tamil Nadu",
+        "google_review_url": "https://g.page/r/CcGeRq4ODL1NEBM/review",
+    },
+    "trivandrum": {
+        "name": "Vilvah Store - Trivandrum",
+        "address": "Trivandrum, Kerala",
+        "google_review_url": "https://g.page/r/CQi8ovYzC1u5EBM/review",
+    },
 }
 
 DEFAULT_STORE = "race-course"
